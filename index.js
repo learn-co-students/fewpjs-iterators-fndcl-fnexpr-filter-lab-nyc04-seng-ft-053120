189bb9cba5a3c9510bb8acbeac244baba56d0e99
peawarrior <jacksonchen171@gmail.com>
2020-08-07 17:50:39 -0400

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function findMatching(array, string) {
     return array.filter((element) => element.toLowerCase() === string.toLowerCase())
 }
 
-
 function fuzzyMatch(array, string) {
     return array.filter((element) => element.toLowerCase().slice(0, string.length) === string.toLowerCase())
 }
